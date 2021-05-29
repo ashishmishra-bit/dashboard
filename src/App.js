7c039cd398dc06b39011d62a843ee3ff7473946a
nks102000 <nitishks1110@gmail.com>
2021-05-29 21:35:32 +0530

    DashBoard Page Made

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 // import logo from './logo.svg';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './Components/Navbar';
-import Home from './pages/Home';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
-  return (   
+  return (
+    <>
       <Router>
         <Navbar />
         <Switch>
-        <Route path ="/" exact component={Home} />               
-        </Switch>
-      </Router>    
+          <Route path="/" exact component={Home} />{" "}
+        </Switch>{" "}
+      </Router>{" "}
+    </>
   );
 }
 
